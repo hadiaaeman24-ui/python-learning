212917a5d09a77e7fb2d44f313e20beab8997168
hadiaaeman24-ui <hadia.aeman24@gmail.com>
2026-03-23 18:56:53 +0100

    Day 2

---
+++ DAY 2 , Variable and input/Day 2 Tasks/A-C level tasks/personcardinfo.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+age = input("What is your age?")
+favouriteColor = input("What is your Favourite color ?")
+print(name +  "is " + age + "year old and loves"+ favouriteColor)
+
+
+firstName = input("What is your first name?")
+lastName = input("What is your last name?")
+fullName = firstName + " " + lastName
+print(fullName)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-learning python with me
+lear python with me in fun way. bY doing task on every step that you learn everthing better 
 <br>
-Day 1 learning print function and some tasks
+Day 1 learning python
+so on day 1 we learned print fuction and the A level task was to write a story by using print
+<br>
+
